a68db4134df92f9e9c17408ebb25ff743803026d
mariorizki <mariorizki07@gmail.com>
2022-11-23 13:16:33 +0700

    add categories component

--- screens/Home.js
+++ screens/Home.js
@@ -2,14 +2,16 @@ import { View, Text, SafeAreaView } from "react-native";
 import React from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
-      <View style={{ backgroundColor: "white", padding: 15 }}>
+      <View style={{ backgroundColor: "#fff", padding: 15 }}>
         <HeaderTabs />
         <SearchBar />
       </View>
+      <Categories />
     </SafeAreaView>
   );
 };
